66b5cca621603d24037cde1f45c8d1ecd33ab9f4
ketchup0211 <dlcka1111@naver.com>
2024-01-03 10:45:20 +0900

    20240103 PS#3

---
+++ Sparta Coding Club/Algorithm Code Kata/exercise27.js
@@ -0,0 +1,7 @@
+function solution(phone_number) {
+  let len = phone_number.length;
+  for (let i = 0; i < len - 4; i++) {
+    phone_number = phone_number.replace(phone_number[i], "*");
+  }
+  return phone_number;
+}
