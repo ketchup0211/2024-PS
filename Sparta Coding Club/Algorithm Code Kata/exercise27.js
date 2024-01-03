c3dc8a21b0eaf4002681a6541e6a0e2c57792cb3
ketchup0211 <dlcka1111@naver.com>
2024-01-03 10:55:34 +0900

    20240103 PS#7

--- Sparta Coding Club/Algorithm Code Kata/exercise27.js
+++ Sparta Coding Club/Algorithm Code Kata/exercise27.js
@@ -1,3 +1,4 @@
+// Programmers 핸드폰 번호 가리기
 function solution(phone_number) {
   let len = phone_number.length;
   for (let i = 0; i < len - 4; i++) {
---
+++ Sparta Coding Club/Algorithm Code Kata/exercise4.js
@@ -0,0 +1,4 @@
+// Programmers 나이 출력
+function solution(age) {
+  return 2022 - age + 1;
+}
