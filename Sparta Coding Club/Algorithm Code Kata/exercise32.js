5fe3492f9e114be4fd148d7d38e82c22d83b69c7
ketchup0211 <dlcka1111@naver.com>
2024-01-08 11:53:06 +0900

    20240108 PS#3

---
+++ Sparta Coding Club/Algorithm Code Kata/exercise32.js
@@ -0,0 +1,20 @@
+// Programmers 내적
+function solution(a, b) {
+  let res = 0;
+  const isSameLength = (arr1, arr2) => {
+    if (arr1.length !== arr2.length) {
+      throw new Error("Different Length");
+    } else return true;
+  };
+  //  error handling
+  try {
+    isSameLength(a, b);
+    for (const idx in a) {
+      res += a[idx] * b[idx];
+    }
+  } catch (err) {
+    console.log(err.message, err.name);
+  } finally {
+    return res;
+  }
+}
