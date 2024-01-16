23f87a6797d077034d148be8dc9026601e0927d3
ketchup0211 <dlcka1111@naver.com>
2024-01-16 20:24:55 +0900

    20240116 PS#1

---
+++ Sparta Coding Club/Algorithm Code Kata/exercise43.js
@@ -0,0 +1,9 @@
+// Programmers 크기가 작은 부분문자열
+function solution(t, p) {
+  let count = 0;
+  for (let i = 0; i <= t.length - p.length; i++) {
+    let value = t.slice(i, i + p.length);
+    if (+p >= +value) count++;
+  }
+  return count;
+}
